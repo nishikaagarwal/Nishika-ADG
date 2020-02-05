a1c0f3ba3c27c78ce30c5f048ca45662df514785
nishikaagarwal <nishika991@gmail.com>
2020-02-05 20:10:43 +0530

    Task-3

--- Fibonacci-Series.playground/Contents.swift
+++ Fibonacci-Series.playground/Contents.swift
@@ -2,13 +2,16 @@ import UIKit
 
 var number1 = 0
 var number2 = 1
-var next = 1
+var next = 0
 
-for _ in 0...5 {
+func fibonacci(n: Int)
+{for _ in 0...n {
+    print(next)
     next = number1 + number2
     number1 = number2
     number2 = next
-    print(next)
     }
-
+}
+print(number1)
+fibonacci(n:5)
 
